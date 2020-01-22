dfe988a08326e344f5cbfe7f4ff80c779842b372
ichthyandr <wimax.peter@gmail.com>
2020-01-22 17:36:48 +0300

    done

--- qtbbutton.cpp
+++ qtbbutton.cpp
@@ -53,30 +53,35 @@ static QMap<int, QString> buttonType = {
 };
 
 static void printRelays( int & X, int & Y, int delta, QPainter & painter, const int number, const int bank){
-    X=0; QString _param("");
+    X=0; QString _param("Relays:   1   2   3   4");
+    Y+= delta;
+    painter.drawText( X, Y, _param );
+    Y+= delta;
     for ( int i=0; i<4; i++ ){
+        switch (i){
+            case 0: X=46; break;
+            case 1: X=60; break;
+            case 2: X=75; break;
+            case 3: X=90; break;
+        }
         uint8_t data    = SSXMSGS::g_BanksSettings[bank].buttonContext[number-1].relays;
         int     res     = (data >> (i*2)) & 0x03;
-        QString _state("Undef");
-
         switch ( res ){
         case RELAY_MASK_NO_ACTION:
-            _state = "No Action";
+            painter.drawText( X, Y, "N" );
             break;
         case RELAY_MASK_CLOSE:
-            _state = "Close";
+            painter.drawText( X, Y, "C" );
             break;
         case RELAY_MASK_OPEN:
-            _state = "Open";
+            painter.drawText( X, Y, "O" );
             break;
         case RELAY_MASK_MOMENTARY:
-            _state = "Momentary";
+            painter.drawText( X, Y, "M" );
             break;
         }
-        Y+= delta;
-        _param = QString("Relay %1: %2").arg(i+1).arg(_state);
-        painter.drawText( X, Y, _param );
     }
+    X=0;
 }
 
 QPixmap QTBbutton::getIcon( const int number, const int bank ){
@@ -88,8 +93,10 @@ QPixmap QTBbutton::getIcon( const int number, const int bank ){
     painter.setFont(f);
     QFontMetrics fm(f);
 
+    int delta = fm.ascent()+2;
+
     int X = 0;
-    int Y = fm.ascent();
+    int Y = delta;
     QString header = QString("Button %1").arg( number );
     painter.drawText( X, Y , header );
 
@@ -101,19 +108,22 @@ QPixmap QTBbutton::getIcon( const int number, const int bank ){
         qCCritical(QTB) << Q_FUNC_INFO << "button number out of range! number=" << number;
         return(pix);
     }
-    QString _ButtonType = buttonType.value( SSXMSGS::g_BanksSettings.at(bank).buttonType[number-1] );
-    Y+= fm.ascent();
-    Y+= fm.ascent();
-    painter.drawText( X, Y, _ButtonType );
-
     char _alias[BUTTON_NAME_MAX_SIZE+1];
     ::memset( _alias, 0, BUTTON_NAME_MAX_SIZE+1);
     ::memcpy( _alias, SSXMSGS::g_BanksSettings.at(bank).buttonContext->nameAlias, BUTTON_NAME_MAX_SIZE);
     QString _aliasName(_alias);
     if (_aliasName.isEmpty()) _aliasName = "Alias Name";
     _aliasName = QString("[%1]").arg(_aliasName);
-    Y+= fm.ascent();
+    Y+= delta;
     painter.drawText( X, Y, _aliasName );
+
+    QString _ButtonType = buttonType.value( SSXMSGS::g_BanksSettings.at(bank).buttonType[number-1] );
+    Y+= delta;
+    f.setBold(true);
+    painter.setFont(f);
+    painter.drawText( X, Y, _ButtonType );
+    f.setBold(false);
+    painter.setFont(f);
 
     QString _param("");
     switch( SSXMSGS::g_BanksSettings.at(bank).buttonType[number-1] ){
@@ -126,7 +136,7 @@ QPixmap QTBbutton::getIcon( const int number, const int bank ){
             }
 
             if (0==(i%2)){
-                X=0; Y+= fm.ascent();
+                X=0; Y+= delta;
             }
             else {
                 X = 60;
@@ -134,7 +144,38 @@ QPixmap QTBbutton::getIcon( const int number, const int bank ){
             _param = QString("m%1: %2").arg(i+1).arg(_subparam);
             painter.drawText( X, Y, _param );
         }
-        printRelays(X,Y,fm.ascent(),painter,number,bank);
+        printRelays(X,Y,delta,painter,number,bank);
+        //IA
+        {
+            uint16_t state = SSXMSGS::g_BanksSettings[bank].buttonContext[number-1].presetChangeContext.iaState;
+            X=0; Y+= delta;
+            painter.drawText( X, Y, "IA:" );
+            X=20;
+            for (int i=0; i<12; i++ ){
+                if((state >> i) & 0x0001)
+                    painter.setBrush(Qt::green);
+                else
+                    painter.setBrush(Qt::gray);
+                painter.drawEllipse(QPointF(X,Y-fm.ascent()/2 + 1), fm.ascent()/3, fm.ascent()/3);
+                X+=(fm.ascent()/3)*2+2;
+            }
+            X=0;
+            /*
+            _param = QString("IA:  %1%2%3%4%5%6%7%8%9%10%11%12")
+                    .arg((state & 0x0001)?"1":"0")
+                    .arg((state>>1 & 0x0001)?"1":"0")
+                    .arg((state>>2 & 0x0001)?"1":"0")
+                    .arg((state>>3 & 0x0001)?"1":"0")
+                    .arg((state>>4 & 0x0001)?"1":"0")
+                    .arg((state>>5 & 0x0001)?"1":"0")
+                    .arg((state>>6 & 0x0001)?"1":"0")
+                    .arg((state>>7 & 0x0001)?"1":"0")
+                    .arg((state>>8 & 0x0001)?"1":"0")
+                    .arg((state>>9 & 0x0001)?"1":"0")
+                    .arg((state>>10& 0x0001)?"1":"0")
+                    .arg((state>>11& 0x0001)?"1":"0"); */
+            //painter.drawText( X, Y, _param );
+        }
         break;
     case SSXMSGS::ButtonType::CC_TOGGLE:
     case SSXMSGS::ButtonType::CC_MOMENTARY:
@@ -142,14 +183,14 @@ QPixmap QTBbutton::getIcon( const int number, const int bank ){
         if (SSXMSGS::ButtonType::CC_TOGGLE==SSXMSGS::g_BanksSettings.at(bank).buttonType[number-1]){
             uint8_t ctl = SSXMSGS::g_BanksSettings[bank].buttonContext[number-1].commonContext.contolAndNrpnChangeContext_.ctrlLsbNumber;
             _param = QString("ctl# %1").arg( (ctl>127)?QString("off"):QString("%1").arg(ctl));
-            Y+= fm.ascent();
+            Y+= delta;
             painter.drawText( X, Y, _param );
 
             uint8_t frz = SSXMSGS::g_BanksSettings[bank].buttonContext[number-1].commonContext.contolAndNrpnChangeContext_.ctrlMsbFreezeNumber;
             _param = QString("frz# %1").arg( (frz>127)?QString("off"):QString("%1").arg(frz));
             painter.drawText( X+60, Y, _param );
 
-            Y+= fm.ascent();
+            Y+= delta;
             _param =
             (SSXMSGS::g_BanksSettings[bank].buttonContext[number-1].commonContext.contolAndNrpnChangeContext_.autoSendState>0)?
                         QString("Send Value"):QString("");
@@ -158,37 +199,44 @@ QPixmap QTBbutton::getIcon( const int number, const int bank ){
         else {
             uint8_t data = SSXMSGS::g_BanksSettings[bank].buttonContext[number-1].commonContext.contolAndNrpnChangeContext_.ctrlLsbNumber;
             _param = QString("Controller# %1").arg( (data>127)?QString("off"):QString("%1").arg(data));
-            Y+= fm.ascent();
+            Y+= delta;
             painter.drawText( X, Y, _param );
-            Y+= fm.ascent();
+            if ( SSXMSGS::ButtonType::CC_CONST_VAL==SSXMSGS::g_BanksSettings.at(bank).buttonType[number-1] ) {
+                data = SSXMSGS::g_BanksSettings[bank].buttonContext[number-1].commonContext.contolAndNrpnChangeContext_.paramLsbOnValue;
+                _param = QString("On Value: %1").arg( (data>127)?QString("off"):QString("%1").arg(data));
+                Y+= delta;
+                painter.drawText( X, Y, _param );
+            }
+            else
+                Y+= delta;
         }
-        printRelays(X,Y,fm.ascent(),painter,number,bank);
+        printRelays(X,Y,delta,painter,number,bank);
         break;
     case SSXMSGS::ButtonType::NRPN_TOGGLE:
     case SSXMSGS::ButtonType::NRPN_MOMENTARY:
     case SSXMSGS::ButtonType::NRPN_CONST_VAL:
-        Y+= fm.ascent();
+        Y+= delta;
         _param = QString("#99: %1").arg(SSXMSGS::g_BanksSettings[bank].buttonContext[number-1].commonContext.contolAndNrpnChangeContext_.ctrlMsbFreezeNumber);
         painter.drawText( X, Y, _param );
         _param = QString("#98: %1").arg(SSXMSGS::g_BanksSettings[bank].buttonContext[number-1].commonContext.contolAndNrpnChangeContext_.ctrlLsbNumber);
         painter.drawText( X +60, Y, _param );
-        Y+= fm.ascent();
+        Y+= delta;
         if ( SSXMSGS::ButtonType::NRPN_MOMENTARY != SSXMSGS::g_BanksSettings.at(bank).buttonType[number-1] ){
             _param = (SSXMSGS::g_BanksSettings[bank].buttonContext[number-1].commonContext.contolAndNrpnChangeContext_.autoSendState>0)?
                         QString("Send Value"):QString("");
             painter.drawText( X, Y, _param );
         }
-        printRelays(X,Y,fm.ascent(),painter,number,bank);
+        printRelays(X,Y,delta,painter,number,bank);
         break;
     case SSXMSGS::ButtonType::NOTE_ON:
     case SSXMSGS::ButtonType::NOTE_OFF:
-        Y+= fm.ascent();
-        Y+= fm.ascent();
+        Y+= delta;
+        Y+= delta;
         _param = QString("Note: %1").arg(
                     SSXMSGS::g_BanksSettings[bank].buttonContext[number-1].commonContext.noteOnConstContext_.noteNumber
         );
         painter.drawText( X, Y, _param );
-        Y+= fm.ascent();
+        Y+= delta;
         _param = QString("Velocity: %1").arg(
                     SSXMSGS::g_BanksSettings[bank].buttonContext[number-1].commonContext.noteOnConstContext_.noteVelocity
         );
@@ -205,15 +253,16 @@ QPixmap QTBbutton::getIcon( const int number, const int bank ){
     case SSXMSGS::ButtonType::BANK_TO:
         _param = QString("Bank number: %1").arg(
                     SSXMSGS::g_BanksSettings.at(bank).buttonContext[number-1].commonContext.bankNumber);
-        Y+= fm.ascent();
-        Y+= fm.ascent();
+        Y+= delta;
+        Y+= delta;
         painter.drawText( X, Y, _param );
+
         break;
     default:;
     }
 
 
-    qCDebug(QTB) << Q_FUNC_INFO << "UPDATED!" << _ButtonType;
+    //qCDebug(QTB) << Q_FUNC_INFO << "UPDATED!" << _ButtonType;
     return(pix);
 }
 
